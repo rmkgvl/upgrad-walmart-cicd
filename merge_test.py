2274e9336055b49c011d1635e2c49b278ba486e0
rmkgvl <rohitkrishna142@gmail.com>
2025-02-03 07:07:55 +0000

    added change from main

--- merge_test.py
+++ merge_test.py
@@ -4,3 +4,6 @@ def print_message():
     print(merge_message)
 
 print_message()
+
+merge_message_2 = "this message is from main branch, this is to test out merge conflict resolving"
+print(merge_message_2)
